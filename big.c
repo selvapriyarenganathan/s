caf96a64d6848dce9b95ddb70b06da3ea8aed701
selvapriyarenganathan <rkavithadev75@gmail.com>
2016-07-28 16:12:16 +0530

    big.c

---
+++ big.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int x,y,z;
+	scanf("%d%d%d",&x,&y,&z);
+	if((x>y)&&(x>z))
+	{
+		printf("%d is greater ",a);
+	}
+	else if((y>x)&&(y>z))
+	{
+		printf("%d is greater",b);
+	}
+	else
+	{
+		printf("%d is greater",c);
+	}
+}
