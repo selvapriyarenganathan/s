09beefc72d915c613ccdbd68da1ccff05cc98e8d
selvapriyarenganathan <rkavithadev75@gmail.com>
2016-07-28 16:00:31 +0530

    v.c

---
+++ v.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char w;
+	int j;
+	char arr[5]={'a','e','i','o','u'};
+	scanf("%c",&w);
+	for(j=0;j<5;j++)
+	{
+	if(w==arr[i])
+	{
+		printf("vowel");
+		break;
+}
+    else
+    {
+    	printf("consonant");
+    	break;
+	}
+	
+	}
+    
+    
+    return 0;
+}
