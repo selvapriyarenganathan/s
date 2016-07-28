7bf78e69a5ef9539a407b14532619317236b4812
selvapriyarenganathan <rkavithadev75@gmail.com>
2016-07-28 15:53:14 +0530

    o.c

---
+++ o.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int e;
+	scanf("%d",&e);
+	if(e!=0)
+	if(e%2==0)
+	printf("even");
+	else
+	printf("odd");
+	else
+	printf("Neither even nor odd");
+	return 0;	
+	
+}
