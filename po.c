0b75a7badfefebe791dea860dd66be358cf9e329
selvapriyarenganathan <rkavithadev75@gmail.com>
2016-07-28 15:42:52 +0530

    po.c

---
+++ po.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a!=0)
+	if(a>0)
+	printf("positive");
+	else
+	printf("negative");
+	else
+	printf("Zero");
+	return 0;
+}
